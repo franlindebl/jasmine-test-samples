54489c9fc8bd01b37b03e99ebd519939b8f5253b
Fran Linde Blázquez <franlindebl@gmail.com>
2019-12-16 17:42:01 +0100

    Added Calculator and WordsCounter

---
+++ calculator.js
@@ -0,0 +1,37 @@
+class Calculator {
+  constructor() {}
+
+  sum(number1, number2) {
+    return number1 + number2;
+  }
+
+  substract(number1, number2) {
+    return number1 - number2;
+  }
+
+  multiply(number1, number2) {
+    return number1 * number2;
+  }
+
+  divide(number1, number2) {
+    return number1 / number2;
+  }
+
+  average(number1, number2) {
+    return (number1 + number2) / 2;
+  }
+
+  factorial(number) {
+    if (number < 0) {
+      throw new Error("There is no factorial for negative numbers");
+    } else if (number == 1 || number == 0) {
+      return 1;
+    } else {
+      return number * this.factorial(number - 1);
+    }
+  }
+}
+
+module.exports = {
+  Calculator
+};
