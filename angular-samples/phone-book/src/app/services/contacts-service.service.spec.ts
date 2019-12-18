b26476fc9d849e5e12ca73cca3e072881451801d
Fran Linde Blázquez <franlindebl@gmail.com>
2019-12-18 17:58:05 +0100

    Added more tests to service

--- angular-samples/phone-book/src/app/services/contacts-service.service.spec.ts
+++ angular-samples/phone-book/src/app/services/contacts-service.service.spec.ts
@@ -1,12 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ContactsServiceService } from './contacts-service.service';
+import { Contact } from '../models/contact';
 
 describe('ContactsServiceService', () => {
+  const fakeContacts: Contact[] = [{
+    id: 1,
+    name: 'Fran',
+    phone: 666555444,
+  }, {
+    id: 2,
+    name: 'Abel',
+    phone: 666555444,
+  }];
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: ContactsServiceService = TestBed.get(ContactsServiceService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return the current contact list', () => {
+    const service: ContactsServiceService = TestBed.get(ContactsServiceService);
+    // @ts-ignore
+    service.contacts = [...fakeContacts];
+    expect(service.getContactList()).toEqual(fakeContacts);
+  });
+
+  it('should save the contact', () => {
+    const service: ContactsServiceService = TestBed.get(ContactsServiceService);
+    // @ts-ignore
+    service.contacts = [...fakeContacts];
+    service.addContactToList('Pepe', 123456789);
+
+    const retrievedContacts: Contact[] = service.getContactList();
+    const expectedContacts: Contact[] = [...fakeContacts, {
+      name: 'Pepe',
+      phone: 123456789,
+      id: 3
+    }];
+
+    expect(retrievedContacts).toEqual(expectedContacts);
   });
 });
